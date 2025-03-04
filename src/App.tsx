407b20f69da1121ea18499f25b170db89fc547ec
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-04 16:37:44 +0500

    using unions itself

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 
-import Bird from "./components/Bird.tsx";
-
 
 const App = () => {
     return (
         <div>
-           <Bird
-               species={"Dog"}
-               canFly={false}/>
+
         </div>
     )
 }
