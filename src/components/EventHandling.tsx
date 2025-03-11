bef683f0da1ed57218d0f6e4dd48f3f17ec8a415
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-11 22:09:48 +0500

    EventHandling end

--- src/components/EventHandling.tsx
+++ src/components/EventHandling.tsx
@@ -1,21 +1,20 @@
-import {useRef} from "react";
 
-const FormInput = () => {
-    const inputRef = useRef<HTMLInputElement>(null);
+import * as React from "react";
 
-    const handleFocus = () => {
-        if(inputRef.current){
-            inputRef.current.focus();
-        }
-        // manually checking instead of optional chaining
-        // inputRef.current?.focus()
+const EventHandling = () => {
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(e.target, "Button Clicked")
     }
-    return (
-        <div>
-            <input type={"text"} ref={inputRef} placeholder={"Enter something"}/>
 
-            <button onClick={handleFocus}>Focus to Input</button>
+    const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+        console.log(e.target, "Mouse Entered")
+    }
+
+    return (
+        <div onMouseEnter={handleMouseEnter}>
+            <h2>Mouse Handle Event</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     )
 }
-export default FormInput
+export default EventHandling
