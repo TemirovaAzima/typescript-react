81dbf9f3fc5e7764168e3247e9153a21dffd79c1
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-11 21:34:33 +0500

    Form end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import UserProfile from './components/UserProfile.tsx'
+import Form from './components/Form';
 
 const App =()=>{
 
     return (
         <div>
-           <UserProfile/>
+          <Form/>
         </div>
     )
 }
