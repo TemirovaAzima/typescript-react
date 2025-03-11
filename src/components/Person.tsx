d2ed81a2f0c003cd6699847c67c276611ff66613
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-11 13:44:34 +0500

    type with useState

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,23 +1,20 @@
 import {useState} from 'react'
-interface PersonProp{
-    name: string;
-    age: number
-}
+
 const Person = () => {
-    // const [user, setUser] = useState<{name: string , age: number}>({
-    //     name: "Azima",
-    //     age: 17
-    // });
-    const [user, setUser] = useState<PersonProp>({
-        name: "Azima",
-        age: 17
-    });
-    const changeUser =()=>setUser({...user, name: "Hedo"})
+    const [items, setItems] = useState<string[]>(["Apple","Banana", "Cherry"])
+    const changeItem =()=>setItems([...items, "Mango"])
+    const deleteItem =()=>setItems(items.filter((item)=>item=== "Mango" ? item !=="Mango": item))
+    const updateInfoItem =()=>setItems(items.map((item)=> item === "Mango" ? "Mango only fav" : item ))
     return (
         <div>
-            {user.name}
-            {user.age}
-            <button onClick={changeUser}>change name </button>
+            {items.map((item: string)=>(
+                <li key={Math.random()}>{item}</li>
+            ))}
+            <button onClick={changeItem}>Add Mango</button>
+            <button onClick={deleteItem}>Delete Mango</button>
+            <button onClick={updateInfoItem}>Update Mango</button>
+
+
         </div>
     )
 }
