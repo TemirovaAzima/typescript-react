5d0c5078a73508a979a1a4f9fcb247851e8fc88b
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-11 15:11:28 +0500

    using both interface and type

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,13 @@
 import {useState} from "react";
-
+// type addFriend =()=>void
+interface addFriend {
+    (): void
+}
 
 const Person = () => {
     const [friends, setFriends] = useState<string[]>(["Lisa", "Jennie"]);
     // const addFriend =(): void=>setFriends([...friends,"Rosie"])
-    const addFriend : ()=>void =()=>setFriends([...friends,"Rosie"])
+    const addFriend : addFriend =()=>setFriends([...friends,"Rosie"])
     return (
         <div>
             {friends.map((friend : string)=>(
