8070630c787d0ab32ae53471547b259351fd9129
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-12 01:04:43 +0500

    weather conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React, {JSX} from "react";
-type PasswordProp={
-    isValid : boolean,
-}
-const ValidPassword=() :JSX.Element=><h1>Right Password</h1>
-const InValidPassword =(): JSX.Element=> <h1>Wrong Password</h1>
-// const Password: React.FC<PasswordProp>=({isValid})=>{
-//     if(isValid){
-//         return ValidPassword()
-//     }else{
-//         return InValidPassword()
-//     }
-// }
+import Weather from "./components/Weather.tsx";
 
-// using ternary operator
-const Password: React.FC<PasswordProp>=({isValid})=>{
-   return  isValid ? <ValidPassword/> : <InValidPassword/>;
-}
 
 const App = () => {
     return (
         <div>
-          <Password isValid={true}/>
+            <Weather temperature={-45}/>
         </div>
     )
 }
