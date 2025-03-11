530d3604f5c60a487905030ea163ac6bc2175ecd
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-11 23:04:14 +0500

    typescript with prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import EventHandling from './components/EventHandling.tsx'
+import React from "react";
+
 
 const App =()=>{
 
     return (
-        <div>
-          <EventHandling/>
-        </div>
+        <main>
+           <User name={"Azima"}/>
+        </main>
     )
 }
 export default App
+type UserProp={
+    name: string
+}
+// const User =(props : {name: string})=>{
+//     console.log(props)
+//     return(
+//         <section>
+//              <h4>{props.name}</h4>
+//         </section>
+//     )
+// }
+// const User =(props : UserProp)=> {
+//     console.log(props)
+//     return (
+//         <section>
+//             <h4>{props.name}</h4>
+//         </section>
+//     )
+// }
+const User: React.FC<UserProp> =(props)=> {
+    console.log(props)
+    return (
+        <section>
+            <h4>{props.name}</h4>
+        </section>
+    )
+}
