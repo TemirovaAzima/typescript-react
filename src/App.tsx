796f2428a5f4233ce2b29733ecd520f8c569763e
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-12 02:11:35 +0500

    todo end

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import Greeting from "./components/Greeting.tsx";
-import {JSX} from "react";
+import React, { useState } from "react";
+
+const TodoList = () => {
+    const [todos, setTodos] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
 
 
-const App = (): JSX.Element => {
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (inputValue.trim()) {
+            setTodos([...todos, inputValue]);
+            setInputValue("");
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
     return (
         <div>
-          <Greeting timeOfDay={"morning"}/>
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder="Add a new todo"
+                />
+                <button type="submit">Add Todo</button>
+            </form>
+            <ul>
+                {todos.map((todo: string, index:number) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
         </div>
-    )
-}
-export default App
+    );
+};
+
+export default TodoList;
