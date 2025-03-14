870c72839caff2720c2fe79649657c0a7e3c5e95
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-14 18:40:57 +0500

    context api typescript

--- src/components/ComponentC.tsx
+++ src/components/ComponentC.tsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import {DataAge, DataName,DataPerson} from '../App.tsx'
-const ComponentC :React.FC = () => {
-    return (
-       <DataName.Consumer>
-           {(name)=>{
-               return(
-                   <DataAge.Consumer>
-                       {(age)=>{
-                           return (
-                               <DataPerson.Consumer>
-                                   {(person)=>
-                                       person? (
-                                           <div>
-                                               <h1>{name ?? "No name provided"}</h1>
-                                               <h1> {age ?? "No age provided"}</h1>
-                                               <h1>{person.name}- {person.age} - {person.isSongOut ? "Yes": "No"}</h1>
-                                           </div>
-                                       ) :(
-                                           <h1>No person data</h1>
-                                       )
-                                   }
-                               </DataPerson.Consumer>
-                           )
-                       }
-                       }
-                   </DataAge.Consumer>
+import {DataAge, DataName, DataPerson} from '../App.tsx'
 
-           )
-           }}
-       </DataName.Consumer>
+const ComponentC: React.FC = () => {
+    return (
+        <DataName.Consumer>
+            {(name) => {
+                return (
+                    <DataAge.Consumer>
+                        {(age) => {
+                            return (
+                                <DataPerson.Consumer>
+                                    {(person) =>
+                                        person ? (
+                                            <div>
+                                                <h1>{name ?? "No name provided"}</h1>
+                                                <h1> {age ?? "No age provided"}</h1>
+                                                <h1>{person.name}- {person.age} - {person.isSongOut ? "Yes" : "No"}</h1>
+                                            </div>
+                                        ) : (
+                                            <h1>No person data</h1>
+                                        )
+                                    }
+                                </DataPerson.Consumer>
+                            )
+                        }
+                        }
+                    </DataAge.Consumer>
+                )
+            }}
+        </DataName.Consumer>
     )
 }
 export default ComponentC
