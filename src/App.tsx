7d04c56049b84682d2209c4489134b3f02d1eabd
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-16 00:32:50 +0500

    useRef focus typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from "react";
+import React, {useRef} from 'react'
 
-const Copy: React.FC = () => {
-    const copyRights = (event: React.ClipboardEvent<HTMLParagraphElement>) => {
-        event.preventDefault();
-        console.log("Text ko‘chirish mumkin emas");
-    };
-
+const App:React.FC = () => {
+    const inputRef = useRef<HTMLInputElement|null>(null);
+    const focusInput = ()=>{
+        // inputRef.current!.focus();
+        // inputRef.current!.value="hehe"
+        if(inputRef.current){
+            inputRef.current.focus();
+            inputRef.current.value="hehe"
+        }
+    }
     return (
-        <p onCopy={copyRights}>
-            Viris purus graeco omittam adolescens movet maximus oporteat viverra efficiantur instructior sententiae
-            posidonium appareat quam pertinax sonet suas maximus alienum penatibus numquam unum congue posidonium
-            molestiae urna consectetur salutatus quidam.
-        </p>
-    );
-};
-
-export const App: React.FC = () => {
-    return (
-        <section>
-            <Copy />
-        </section>
-    );
-};
-
-export default App;
+        <div>
+            <input ref={inputRef} type="text" placeholder="Enter User Name"/>
+            <button onClick={focusInput}>focus input</button>
+        </div>
+    )
+}
+export default App
