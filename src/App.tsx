ea5c3f53f79765431d1dce2e3a8c3761f16cf6ae
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-15 21:39:29 +0500

    if else useEffect typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import {TodoList} from "./components/TodoList.tsx";
+import React, {useEffect, useState} from 'react'
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+    const [value, setValue] = useState<number>(0);
+    const [smth, setSmth] = useState<number>(1)
+
+    useEffect(() => {
+        if (value > 3) {
+            console.log("UseEffect hook");
+            document.title = `Increment ${value} ${smth}`;
+        }
+    }, [value, smth]); // dependency array to prevent unnecessary re-renders
+
     return (
         <div>
-          <TodoList/>
+            <h2>{value}</h2>
+            <h2>{smth}</h2>
+            <button onClick={() => setValue(value + 1)}>Increment</button>
+            <button onClick={() => setSmth(smth + 1)}>Increment</button>
         </div>
-    )
-}
+    );
+};
 export default App
