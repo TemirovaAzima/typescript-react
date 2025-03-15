eace34554c6a214b6e0906d9e6f61226909f2d24
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-16 03:20:46 +0500

    useId by using map typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import React, {useRef, useState} from 'react'
-
+import React, {useId} from 'react'
+const items: string[] = ["Apple","Banana","Orange"];
 const App:React.FC = () => {
-    const [message,setMessage]=useState<string>("Waiting...");
-    const timeoutRef = useRef<null|number>(null);
-    const startTimeout = ()=>{
-        setMessage("Waiting...");
-        timeoutRef.current = setTimeout(()=>{
-            setMessage("Timeout finished");
-        },2000);
-    };
-
-    const cancelTimeout = ()=>{
-        if(timeoutRef.current !==null){
-            clearTimeout(timeoutRef.current);
-            timeoutRef.current = null;
-            setMessage("Timeout canceled")
-        }
-    }
+    const idPrefix = useId(); // Generates a unique prefix
     return (
         <div>
-            <h2>{message}</h2>
-            <button onClick={startTimeout}>Start Timeout</button>
-            <button onClick={cancelTimeout}>Cancel Timeout</button>
+           <ul>
+               {items.map((item,index)=>{
+                   const id = `${idPrefix} -${index}`;
+                   return <li key={id} id={id}>{item}</li>
+               })}
+           </ul>
         </div>
-    )
-}
+    );
+};
 export default App
